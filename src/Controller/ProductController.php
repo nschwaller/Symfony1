36c9ff7c884671f8429156a595a2151377a5ffb3
nschwaller <nadia1.schwaller@gmail.com>
2021-04-24 01:27:13 +0200

    Commentaire pour afficher le produit séléctionner
    - Récupérer un produit en fonction d'un paramètre du produit
    - Ajouter un paramètre dans un lien

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -45,12 +45,17 @@ class ProductController extends AbstractController
         ]);
     }
 
+    //Paramètre qui va nous permettre d'afficher le produit en fonction de son nom c'est comme si on affiichait l'id du produit mais c'est plus sécurisé car c'est son slug
     #[Route('/produit/{slug}', name: 'products')]
+    //on récupère le paramètre de notre route
     public function show($slug): Response
     {
+        //Récupère dans la base de données le prduit qui a le slug equivalent au paramètre slug (select * from product where slug = {slug})
         $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
 
+        //si le produit est pas trouvé par exemple qu'un utilisateur marque n'importe quoi, on redirige vers tout les produits
         if(!$product){
+            //équivalent d'un header
             return $this->redirectToRoute('product');
         }
         return $this->render('product/show.html.twig',[
